13d98af7a964958bd92c5994bd757ed6d95aa35f
Alessandro <alessandro.lekkas@startmail.com>
2019-06-05 19:33:15 +0200

    New method for drawing arcs for links based on distance, WIP

--- frontend/src/components/DataList.js
+++ frontend/src/components/DataList.js
@@ -5,6 +5,7 @@ export default class DataList extends React.Component {
 
   state = {
     data: [],
+    server : null,
   };
 
   render() {
@@ -28,29 +29,3 @@ export default class DataList extends React.Component {
     )
   }
 }
-/*
-[
-    {
-        "country": "Italy",
-        "city": "Busto Arsizio",
-        "ip": "130.25.193.243",
-        "terminals": [
-            [
-                "5136",
-                "lee",
-                "pts"
-            ],
-            [
-                "5855",
-                "lee",
-                "notty"
-            ]
-        ],
-        "cartesian": [
-            38.015776875757425,
-            5.918516250776332,
-            39.30358601860552
-        ]
-    }
-]
- */
--- frontend/src/components/Visualisation.js
+++ frontend/src/components/Visualisation.js
@@ -14,25 +14,44 @@ export default class Visualisation extends React.Component {
     super(props);
     this.scene = new GlobeScene();
     this.dataListRef = React.createRef();
+    this.fetchServer();
     this.fetchData();
     setInterval(this.fetchData, UPDATE_TIME);
   }
 
-
-  fetchData = () => {
-    fetch(`${BACKEND_URL}/data?radius=` + (EARTH_RADIUS)).then(resp => {
-        return resp.json();
+  fetchServer = () => {
+    fetch(`${BACKEND_URL}/server?radius=` + (EARTH_RADIUS)).then(resp => {
+        return testSv;//todo test
       }
     ).then(jsonRes => {
       if(jsonRes.error){
-        swal("Error", jsonRes.error,"error")
-        console.log(jsonRes.error)
+        swal("Error", jsonRes.error, "error");
+        console.log(jsonRes.error);
         return;
       }
       console.log(jsonRes);
-      this.scene.displayData(jsonRes);
-      this.dataListRef.current.setState({data: jsonRes});
+      this.scene.displayServer(jsonRes);
+      this.dataListRef.current.setState({server: jsonRes});
       // this.setCameraTarget(0);
+    }).catch(error => {
+      console.error(error)
+    });
+  };
+
+  fetchData = () => {
+    fetch(`${BACKEND_URL}/data?radius=` + (EARTH_RADIUS)).then(resp => {
+        return testArr;//todo test
+      }
+    ).then(jsonRes => {
+      if(jsonRes.error){
+        swal("Error", jsonRes.error, "error");
+        console.log(jsonRes.error);
+        return;
+      }
+      // console.log(jsonRes);
+      let result = this.scene.displayData(jsonRes);
+      //todo switch to sockets
+      this.dataListRef.current.setState({data: jsonRes});
     }).catch(error => {
       console.error(error)
     });
@@ -60,3 +79,77 @@ export default class Visualisation extends React.Component {
     )
   }
 }
+var testSv =
+  {
+    "country": "France",
+    "city": "Gravelines",
+    "cartesian": [
+      34.5984226367739,
+      1.2841100474131453,
+      42.735233852563674
+    ],
+    "proxy": false
+  };
+
+var testArr =
+  [
+    {
+      "country": "Op1",
+      "city": "City1",
+      "ip": "66.66.66.66",
+      "terminals": [
+        [
+          "5136",
+          "lee",
+          "pts"
+        ],
+        [
+          "5855",
+          "lee",
+          "notty"
+        ]
+      ],
+      "cartesian": [
+        // 27.5,
+        // 0,
+        // 47.63139721
+        -34.5984226367739,
+        -1.2841100474131453,
+        -42.735233852563670
+      ]
+    },
+    {
+      "country": "Op2",
+      "city": "City2",
+      "ip": "33.33.33.33",
+      "terminals": [
+        [
+          "5136",
+          "lee",
+          "pts"
+        ]
+      ],
+      "cartesian": [
+        27.5,
+        0,
+        47.63139721
+      ]
+    },
+    {
+      "country": "Op3",
+      "city": "City3",
+      "ip": "99.99.99.99",
+      "terminals": [
+        [
+          "5136",
+          "lee",
+          "pts"
+        ]
+      ],
+      "cartesian": [
+        -42.13244437,
+        -35.35331853,
+        0
+      ]
+    }
+  ];
