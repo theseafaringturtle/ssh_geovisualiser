8008c4fa0b829c4f0793689271ffd0e3dff5543b
Alessandro <alessandro.lekkas@startmail.com>
2019-06-10 18:14:26 +0200

    Account for local connections, add server location, various fixes

--- backend/start.sh
+++ backend/start.sh
@@ -3,5 +3,5 @@
 CLIENT_URL="http://127.0.0.1:3000"
 
 sudo echo "Starting..."
-sudo python3.7 local/local.py | python3.7 start.py $CLIENT_URL
+sudo python3 local/local.py | python3 start.py $CLIENT_URL
 
